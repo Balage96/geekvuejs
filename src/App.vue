4a7094562d9a744f15641a092e8da475d498aa2f
Balage96 <36931987+Balage96@users.noreply.github.com>
2019-11-07 11:30:26 +0100

    g54h45h46hh
    343gg34f34f3g

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,6 @@ export default {
               {name: 'Hitoshi', speciality: 'Click Events', show: false},
               {name: 'Tango', speciality: 'Conditionals', show: false},
               {name: 'Kami', speciality: 'Webpack', show: false},
-              {name: 'Yoshi', speciality: 'Data Diggin', show: false},
-                 {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-              {name: 'Hitoshi', speciality: 'Click Events', show: false},
-              {name: 'Tango', speciality: 'Conditionals', show: false},
               {name: 'Kami', speciality: 'Webpack', show: false},
               {name: 'Yoshi', speciality: 'Data Diggin', show: false}
           ],
